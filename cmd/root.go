9041d047d99da7c1ba87dbfc4344b29ec9d546a6
Aurelie Vache <scraly@gmail.com>
2024-01-11 15:46:16 +0000

    init

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "prog-mag-cli-go",
+	Short: "Gophers CLI",
+	Long: `Gophers CLI allows you to call the Gophers API. It allows to:
+
+    - list the existing Gophers
+    - display the information about a Gopher
+    - create a new Gopher
+    - delete a Gopher
+    - update the path and the URL of a Gopher`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.prog-mag-cli-go.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
